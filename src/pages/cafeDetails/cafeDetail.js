bc1c920923b9f35b585a661730bfa3c29cd83208
khw00185 <khw0185@gmail.com>
2024-06-07 07:04:57 +0900

    댓글 작성시 바로 반영 2차

--- src/pages/cafeDetails/cafeDetail.js
+++ src/pages/cafeDetails/cafeDetail.js
@@ -21,6 +21,7 @@ function CafeDetail() {
     const cafeId = location.state?.cafeId;
     const token = sessionStorage.getItem('accessToken')
     const [loginModalOpen, setLoginModalOpen] = useState(false);
+    const [pageReLoad, setPageReLoad] = useState(false);
 
     const [cafeData, setCafeData] = useState({
         cafePhotoUrl: "",
@@ -195,7 +196,7 @@ function CafeDetail() {
                     </div>
                     <div className={style.reviewHRContainer}><hr className={style.reviewHR} /></div>
                     <ul>
-                        {cafeData.bestReviewList.map((data) => (<ReviewList key={data.reviewId} props={data} />))}
+                        {cafeData.bestReviewList.map((data) => (<ReviewList key={data.reviewId} props={data} pageReLoad={pageReLoad} setPageReLoad={setPageReLoad}/>))}
                     </ul>
                     <div className={style.reviewPlus}>
                         <span onClick={moveMoreReview}>리뷰 더보기</span>
